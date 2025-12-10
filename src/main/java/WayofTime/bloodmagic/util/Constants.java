89a4097bf5c2177904cb8fcff0faba61d7e75026
Viptunbeqwfwew <92296042+Viptunbeqwfwew@users.noreply.github.com>
2025-12-10 15:40:14 +0900

    Bugfix: Исправлен инвертированный ритуальный камень. После перезахода в одиночный мир или перезапуска сервера, он работал как главный ритуальный камень. Из-за того, что он не восстанавливал значения переменной inverted в TileMasterRitualStone.

--- src/main/java/WayofTime/bloodmagic/util/Constants.java
+++ src/main/java/WayofTime/bloodmagic/util/Constants.java
@@ -27,6 +27,7 @@ public class Constants {
         public static final String IS_RUNNING = "isRunning";
         public static final String IS_REDSTONED = "isStoned";
         public static final String RUNTIME = "runtime";
+        public static final String IS_INVERTED = "isInverted";
         public static final String DIRECTION = "direction";
         public static final String REAGENT_TANKS = "reagentTanks";
         public static final String CURRENT_INCENSE = "BM:CurrentIncense";
